3b4c4b5e738984dbf1bbd6d7fa9e691697301393
bytedissident <dbronston@me.com>
2016-10-01 17:43:32 -0400

    Initial Commit

---
+++ BDAlgos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BDAlgos
+//
+//  Created by derek lee bronston on 10/1/16.
+//  Copyright © 2016 Bytedissident. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
