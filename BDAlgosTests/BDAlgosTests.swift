c95542e564d74b3b088c8d7f85068eb33b68842b
bytedissident <dbronston@me.com>
2016-10-01 18:59:46 -0400

    tests added

--- BDAlgosTests/BDAlgosTests.swift
+++ BDAlgosTests/BDAlgosTests.swift
@@ -11,6 +11,7 @@ import XCTest
 
 class BDAlgosTests: XCTestCase {
     
+    let sut:BDAlgos = BDAlgos()
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,17 +21,32 @@ class BDAlgosTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testMergeSort(){
+        
+        let result = sut.mergeSort(array:[10,8,2,12,44])
+        XCTAssertEqual(result[0], 2)
+        XCTAssertEqual(result[1], 8)
+        XCTAssertEqual(result[2], 10)
+        XCTAssertEqual(result[3], 12)
+        XCTAssertEqual(result[4], 44)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testCountingSort(){
+        
+        let result = sut.countingSort(array:[10,8,2,12,44],max:100)
+        XCTAssertEqual(result[0], 2)
+        XCTAssertEqual(result[1], 8)
+        XCTAssertEqual(result[2], 10)
+        XCTAssertEqual(result[3], 12)
+        XCTAssertEqual(result[4], 44)
     }
     
+    
+    func testBinarySearch(){
+        
+        let result = sut.binarySearch(target: 2, array: [10,8,2,12,44])
+        XCTAssertTrue(result);
+    }
 }
